ff40380a442e6cece0e4c140ea78311a16232963
Andrei Leonte <leonteiandrei@gmail.com>
2021-12-05 22:16:43 +0200

    Start working on configurating the duede server

--- source/Reminders/YRM.IdentityServer.Web/Program.cs
+++ source/Reminders/YRM.IdentityServer.Web/Program.cs
@@ -1,14 +1,11 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Identity.Web;
+using YRM.Domain.Entities.Identity;
+using YRM.IdentityServer.Web;
 using YRM.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
-
-builder.Services
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -19,6 +16,28 @@ builder.Services.AddDbContext<ReminderDbContext>(
     options =>
         options.UseSqlServer(
             configuration.GetConnectionString("ReminderDBConnectionString")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ReminderDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services
+    .AddIdentityServer(options =>
+    {
+        options.Events.RaiseErrorEvents = true;
+        options.Events.RaiseInformationEvents = true;
+        options.Events.RaiseFailureEvents = true;
+        options.Events.RaiseSuccessEvents = true;
+        options.EmitStaticAudienceClaim = true;
+    })
+    //.AddInMemoryIdentityResources(Config.IdentityResources)
+    //.AddInMemoryApiScopes(Config.ApiScopes)
+    //.AddInMemoryClients(Config.Clients)
+
+    .AddInMemoryClients(builder.Configuration.GetSection("ReminderIdentityClients"))
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddAspNetIdentity<ApplicationUser>();
 
 var app = builder.Build();
 
@@ -34,5 +53,6 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseIdentityServer();
 
 app.Run();
